94c0813f52508b3509e1cb0d57ab984800fb4090
Aghost <youri_van_sorgen2@example.com>
2021-08-15 06:45:49 +0200

    add docker to bruh

--- blank-bruh/app.py
+++ blank-bruh/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import Flask, Response, send_from_directory
 
 app = Flask('app', static_url_path='')
@@ -11,13 +12,10 @@ def hello_world():
     response = Response()
     response.headers['link'] = '<style.css>; rel=stylesheet;'
 
-    # RickRoll
-    #response.headers['Refresh'] = '5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-
     # Bruh
-    response.headers['Refresh'] = '5; url= https://www.youtube.com/watch?v=D2_r4q2imnQ'
+    #response.headers['Refresh']='1; url=https://www.youtube.com/watch?v=D2_r4q2imnQ'
 
     return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
